e99d0e242a0f717a6194cb22ac4faecf2ea5de02
OUSSAMA WACHOFI <148217915+WchOussama@users.noreply.github.com>
2025-01-24 16:37:10 +0100

    add variable definitions for Terraform

---
+++ foundation/variables.tf
@@ -0,0 +1,29 @@
+variable "region" {
+  description = "La région où déployer les ressources"
+  type        = string
+  default     = "fr-par"
+}
+
+variable "zone" {
+  description = "La zone où déployer les ressources"
+  type        = string
+  default     = "fr-par-1"
+}
+
+variable "project_id" {
+  description = "calculatrice native"
+  type        = string
+  default     = "123e4567-e89b-12d3-a456-426614174000"
+}
+
+variable "nombinome1" {
+  description = "variable nombinome1"
+  type        = string
+  default     = "WACHOFI"
+}
+
+variable "nombinome2" {
+  description = "variable nombinome2"
+  type        = string
+  default     = "OUALI"
+}
